8fbbcb11c0db4af3dce03f37d18de71d875c954d
Iconews <pedrocasella27@gmail.com>
2020-09-01 13:43:54 -0300

    small update

--- js/projetos.js
+++ js/projetos.js
@@ -1,17 +1,26 @@
-function mudaImg2(){
-
-    document.getElementById("project").src = "projetos/Guikla PAZ E_SPORTS POMBA.png"
-}
-
 function mudaImg1(){
 
     document.getElementById("project").src = "projetos/Guilda PAZ E_SPORTS.png"
+}
+
+
+function mudaImg2(){
+
+    document.getElementById("project").src = "projetos/Guikla PAZ E_SPORTS POMBA.png"
 }
 
 function mudaImg3(){
 
     document.getElementById("project").src = "projetos/Guikla PAZ E_SPORTS sharingan.png"
 }
+
+function mudaImg4() {
+
+    document.getElementById("project").src = "projetos/Fundo Guerra GUILDA PAZ_E-SPORTS.png"
+}
+
+
+
 
 function naoDisponivel() {
 
