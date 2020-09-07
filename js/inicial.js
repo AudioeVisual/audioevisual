b0031a6cfd403b7bad0f876879391e29ba1b4b92
Iconews <pedrocasella27@gmail.com>
2020-09-07 19:16:55 -0300

    small update

--- js/inicial.js
+++ js/inicial.js
@@ -3,3 +3,13 @@ function efeitoBotao() {
     document.getElementById("butinit")
 
 }
+
+function trocarFlix() {
+
+    document.getElementById("cursoflix").src = "images/cursos/cursoflix/cursoflixbarhover.png"
+}
+
+function voltarFlix() {
+
+    document.getElementById("cursoflix").src = "images/cursos/cursoflix/cursoflixbar1.png"
+}
