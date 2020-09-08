f8300d73da7f47bcae5faa84b694ffd4986aaf4b
Iconews <pedrocasella27@gmail.com>
2020-09-07 21:27:57 -0300

    small update

--- js/inicial.js
+++ js/inicial.js
@@ -13,3 +13,24 @@ function voltarFlix() {
 
     document.getElementById("cursoflix").src = "images/cursos/cursoflix/cursoflixbar1.png"
 }
+
+function icoDesignMudaMarketing() {
+
+    document.getElementById("marketingico").src = "ico/designabahover.png";
+}
+
+function icoDesignMudaDesign() {
+
+    document.getElementById("designico").src = "ico/marketingabahover.png";
+}
+
+function icoDesignVoltaMarketing() {
+
+    document.getElementById("marketingico").src = "ico/designaba.png";
+
+}
+
+function icoDesignVoltaDesign() {
+
+    document.getElementById("designico").src = "ico/marketingaba.png";
+}
